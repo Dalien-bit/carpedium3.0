7731e197fdd8e4d04076a2d9b5dfd5a321d48011
samSoumyajitD <92439710+samSoumyajitD@users.noreply.github.com>
2023-01-16 12:33:00 +0530

    sponser area( Soumyajit)

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -3,124 +3,68 @@ import "./styles.css";
 
 const Sponsors = () => {
   return (
-    <div className="bg-black">
-      <p className="text-white text-3xl text-center py-10">Our proud partners</p>
-      <div class="marquee">
-        <div class="marquee-content">
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=01"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=02"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=03"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=04"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=05"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=06"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=07"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=08"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=01"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=02"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=03"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=04"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=05"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=06"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=07"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://via.placeholder.com/600/000000/FFFFFF/?text=08"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    <div class="marquee-full-width">
+                <h1 class="tspon">OUR SPONSORS</h1>
+                   
+                    <div class="marquee-box">
+                        <div class="marquee">
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/sanjeevkapooracademy-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Sanjeev kapoor Academy</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/tutionfe-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Tuition Fe by BSE</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/symbiosistbi-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Symboisis Centre For Entrepreneurship & Innovation</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/srimantha-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Srimantha</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/gurukul-100x-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Gurukul by 100X VC</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/shikshamaitri-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Shikshamaitri</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/edifypath-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Edifypath</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/iesd-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Indian Energy Skill Development by UPES</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/meghnaddesaiacademy-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Meghnad Desai Academy Of Economics</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/beamusician-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Beamusician</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/fundenable-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Fundenable</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="https://knorish.com/assetsnew/images/client-logos/bingeclass-logo-01.jpg" alt="featured creator"/>
+                                <figcaption>Binge Marketing by Gopal Krishnan</figcaption>
+                            </figure>
+                            
+                        </div>
+                        
+                            
+                            
+                        </div>
+                    </div>
+                
+            
   );
 };
 
