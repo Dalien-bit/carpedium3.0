e1c8d001357a6546540aa6899d05fc4b075ff108
krish <kc749335@gmail.com>
2023-01-18 19:43:33 +0530

    added ecell page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Event1 from './components/event/Event1';
-import Event2 from './components/event/Event2';
-import Event3 from './components/event/Event3';
+
 
 function App() {
   return (
     <Routes >
-      <Route path='event1' element={<Event1 />} />
-      <Route path='event2' element={<Event2 />} />
-      <Route path='event3' element={<Event3 />} />
+      
+      <Route path='/home' element={<Home />} />
       <Route path='/*' element={<Home />} />
     </Routes>
   );
