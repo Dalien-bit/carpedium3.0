0b5d4d8eebbab94b71678290bfe0220fd2c9b73e
Pranjal tank <78424052+pranjal-tank@users.noreply.github.com>
2023-01-19 18:07:00 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
       </div>
       <div className="card flex m-auto lg:w-1/2 lg:h-1/2">
         <div className="card2">
-          <h1 className="text-white pl-10 text-5xl pt-5">About Carpedium</h1>
+          <h1 className="text-white pl-10 text-5xl pt-5">About Carpediem</h1>
           <p className="text-white px-14 py-12">{about}</p>
         </div>
       </div>
